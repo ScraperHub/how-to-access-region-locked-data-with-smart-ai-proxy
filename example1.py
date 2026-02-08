18a95fc6daf72299a9407e960e6280a6d7909dca
Scraper Hub <marketing@crawlbase.com>
2026-02-09 06:39:05 +0800

    Add Crawlbase Smart Proxy examples with region-locked data access
    Includes two examples demonstrating how to access region-locked data using Crawlbase Smart Proxy with country and zipcode targeting.

--- example1.py
+++ example1.py
@@ -4,7 +4,7 @@ from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-input_url = "https://www.amazon.com/s?k=iphone+17&crid=2LCG4QYCPJYFV&sprefix=iphone+17%2Caps%2C430&ref=nb_sb_noss_2"
+input_url = "https://www.amazon.com/Sony-WH-1000XM5-Canceling-Headphones-Hands-Free/dp/B09XS7JWHH/ref=sr_1_1"
 
 private_access_token = "YOUR_CRAWLBASE_TOKEN"
 proxy_url = f"http://{private_access_token}:@smartproxy.crawlbase.com:8012"  # Use https:// and port 8013 for HTTPS
@@ -13,7 +13,7 @@ proxies = {
     "https": proxy_url
 }
 crawlbase_api_parameters = {
-    "country": "GB",
+    "country": "US", #GB
 }
 
 try:    
@@ -28,10 +28,10 @@ try:
     
     print('Response Code:', response.status_code)
     
-    # Save response to output.html
-    with open('example1.html', 'w', encoding='utf-8') as f:
+    output_file_name = f"example1-{crawlbase_api_parameters['country']}.html"
+    with open(output_file_name, 'w', encoding='utf-8') as f:
         f.write(response.text)
     
-    print('Response saved to example1.html')
+    print(f'Response saved to {output_file_name}')
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
--- example2.py
+++ example2.py
@@ -4,7 +4,7 @@ from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-input_url = "https://www.amazon.com/s?k=iphone+17&crid=2LCG4QYCPJYFV&sprefix=iphone+17%2Caps%2C430&ref=nb_sb_noss_2"
+input_url = "https://www.amazon.com/Mount-Comfort-Coffee-Organic-Whole/dp/B07171HMF5/ref=sr_1_2"
 
 private_access_token = "YOUR_CRAWLBASE_TOKEN"
 proxy_url = f"http://{private_access_token}:@smartproxy.crawlbase.com:8012"  # Use https:// and port 8013 for HTTPS
@@ -14,7 +14,7 @@ proxies = {
 }
 crawlbase_api_parameters = {
     "country": "US",
-    "zipcode": "90210",
+    "zipcode": "90210", #10004
 }
 
 try:    
@@ -29,10 +29,10 @@ try:
     
     print('Response Code:', response.status_code)
     
-    # Save response to output.html
-    with open('example2.html', 'w', encoding='utf-8') as f:
+    output_file_name = f"example2-{crawlbase_api_parameters['country']}-{crawlbase_api_parameters['zipcode']}.html"
+    with open(output_file_name, 'w', encoding='utf-8') as f:
         f.write(response.text)
     
-    print('Response saved to example2.html')
+    print(f'Response saved to {output_file_name}')
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
